e517344c34a2cceb3ba7f4b5e66b134282d2b196
Florian Bruhin <me@the-compiler.org>
2024-10-18 10:12:10 +0200

    Just use cwd

--- genslides.py
+++ genslides.py
@@ -1,11 +1,9 @@
-import shutil
 import subprocess
-import tempfile
 from pathlib import Path
 from jinja2 import Environment
 
 
-TALKS: list[tuple[str, str, str | None]] = [
+TALKS: list[tuple[str, str, str]] = [
     ("Speaker 1", "Title 1", "testfile1.pdf"),
     ("Speaker 2", "Title 2", "testfile2.pdf"),
     ("Speaker 3", "Title 3", "testfile3.pdf"),
@@ -18,7 +16,7 @@ TALKS: list[tuple[str, str, str | None]] = [
 
 def main() -> None:
     talks = [
-        (speaker, title, Path(pdf).resolve() if pdf else None, next_speaker)
+        (speaker, title, pdf, next_speaker)
         for (speaker, title, pdf), (next_speaker, _, _) in zip(
             TALKS, TALKS[1:] + [("", "", None)]
         )
@@ -36,16 +34,10 @@ def main() -> None:
     template = env.from_string(Path("genslides.tex.j2").read_text())
     tex_src = template.render(entries=talks)
 
-    with tempfile.TemporaryDirectory() as tempdir:
-        tempdir_path = Path(tempdir)
-        tex_file_path = tempdir_path / "talks.tex"
+    tex_file_path = Path("talks.tex")
+    tex_file_path.write_text(tex_src)
 
-        tex_file_path.write_text(tex_src)
-
-        subprocess.run(["latexmk", "-pdf", tex_file_path], check=True, cwd=tempdir_path)
-        final_pdf_path = tex_file_path.with_suffix(".pdf")
-        output_pdf_path = Path.cwd() / final_pdf_path.name
-        shutil.copy(final_pdf_path, output_pdf_path)
+    subprocess.run(["latexmk", "-pdf", "-shell-escape", tex_file_path], check=True)
 
 
 if __name__ == "__main__":
