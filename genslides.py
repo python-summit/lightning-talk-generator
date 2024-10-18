ab98cd5f6af743c9cad77d19d712bf65ad3666d0
Florian Bruhin <me@the-compiler.org>
2024-10-18 15:34:34 +0200

    update

--- genslides.py
+++ genslides.py
@@ -4,17 +4,17 @@ from jinja2 import Environment
 
 
 TALKS: list[tuple[str, str, str | None]] = [
-    ("Stefan Keller", "Pythons and Ducks!", "Lightning_Talk_Stefan_Keller_Pythons_and_Ducks.pdf"),
     ("Dominik Gresch", "Better Autocomplete with Type Hints", "better_autocomplete_with_type_hints.pdf"),
-    ("Tamara Reuveni Mazig", "Topic Modelling for Customer experience", None),  # FIXME
-    ("Jan Werth", r"AI \& our Brain $\rightarrow$ not quite there yet", None),  # FIXME
+    ("Tamara Reuveni Mazig", "Topic Modelling for Customer experience", "Lightning Talk Tamara Reuveni Mazig.pptx.pdf"),
+    # ("Jan Werth", r"AI \& our Brain $\rightarrow$ not quite there yet", None),  # FIXME
     ("Simon Niederberger", r"Jupyter \& Git", "presentation.pdf"),
     ("Vita Midori", "ML at Demokratis.ch", "Vita Midori - ML at Demokratis.pdf"),
-    ("Tim Head", "Zero Code Change Acceleration", None),  # FIXME
+    ("Tim Head", "Zero Code Change Acceleration", 'Lightning - Zero Code Change.pptx.pdf'),
+    # ("Timon Erhart", "Data Dashboard in 5min", None),
+    ("Josua Schmid", "Funny Slides for Data Scientist", "2024-10-18 Funny Slides for Data Scientists.pdf"),
+    ("Ricardo Pereira", "Python Superset", "PythonSuperset.pdf"),
+    ("Stefan Keller", "Pythons and Ducks!", "Lightning_Talk_Stefan_Keller_Pythons_and_Ducks.pdf"),
     # ("Florian Bruhin", "fstring.help", None),  # FIXME
-    # ("Timon Erhart", "Data Dashboard in 5min", None),  # FIXME
-    # ("Josua Schmid", "Funny Slides for Data Scientist", "2024-10-18 Funny Slides for Data Scientists.pdf"),
-    # (..., ..., ...)
     ("Florian Bruhin", "A .py/.tex/.pdf quine", None),
 ]
 
